cad118103fd699c4d05a6ad1997ef39f4c141274
Kanha9937-glitch <subham9937128146@gmail.com>
2026-09-25 08:29:48 +0530

    Create 3_Palindrom_Number.py

---
+++ solutions/3_Palindrom_Number.py
@@ -0,0 +1,15 @@
+# Problem: Palindrome Numbers
+# Difficulty: Easy
+# Language: Python
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        original = x
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+
+        return original == reverse
